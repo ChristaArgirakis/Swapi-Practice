c54d0792e67de8731ca38675cab27b881c9901c9
Christa Argirakis <cargigrace@gmail.com>
2023-06-04 15:35:11 -0400

    Edits to HTML, complete CSS, start of JS

---
+++ main.js
@@ -0,0 +1,9 @@
+let searchInput = document.getElementById('search');
+
+document.getElementById('get-character').addEventListener('click', function() {
+
+});
+
+document.getElementById('get-all').addEventListener('click', function() {
+
+});
